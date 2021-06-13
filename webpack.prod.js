2fd155a8162c5b21f6afd380c7f2af36a81653a5
LostInStatic <kowal.a1234@gmail.com>
2021-06-14 00:32:39 +0200

    dependency updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const extractCss = require('mini-css-extract-plugin');
-const miniCss = require('optimize-css-assets-webpack-plugin');
+const miniCss = require('css-minimizer-webpack-plugin');
 const miniJs = require('terser-webpack-plugin');
 
 
