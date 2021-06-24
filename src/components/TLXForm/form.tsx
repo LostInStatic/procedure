9a7d80aad726db4ae375e17b082ea65159c232a4
LostInStatic <kowal.a1234@gmail.com>
2021-06-24 12:24:28 +0200

    TLX form base components

--- src/components/TLXForm/form.tsx
+++ src/components/TLXForm/form.tsx
@@ -1,14 +1,72 @@
+import { Button } from '@material-ui/core';
 import React = require('react');
+import { useDataLogger } from '../data/dataLogger';
+import TLXInput from './input';
 
 interface Props {
 	
 }
 
-const name: React.FC<Props> = (props) => {
+const TLXForm: React.FC<Props> = (props) => {
+	const logger = useDataLogger();
 
-	
+	const [values, updateValues] = React.useReducer(
+		(state, change: { value: number, id: string})  => {
+			state[change.id] = change.value;
+			return {...state};
+		},
+		{}
+	);
 
-	return <></>;
+
+	return <>
+		{questions.map(
+			question => <TLXInput
+				key={question.id}
+				label={question.label}
+				axisLabels={question.axisLabels}
+				onChange={(e, value) => updateValues({value: value, id: question.id})}
+			/>
+		)}
+		<Button
+			variant='contained'
+			color='primary'
+			className="next-button"
+			onClick={
+				() => {
+					console.log(values);
+				}
+			}
+		>Zakończ</Button>
+	</>;
 };
 
-export default name;
+export default TLXForm;
+
+interface Question {
+	id: string
+	label: string,
+	axisLabels: {
+		min: string,
+		max: string
+	}
+}
+const questions: Question[] = [
+	{
+		id: 'Test',
+		label: 'Test question?',
+		axisLabels: {
+			min: 'min',
+			max: 'max'
+		}
+	},
+	{
+		id: '2',
+		label: 'Two?',
+		axisLabels: {
+			min: 'XD',
+			max: 'DX'
+		}
+	}
+
+];
---
+++ src/components/TLXForm/input.tsx
@@ -0,0 +1,48 @@
+import { Grid, Slider } from '@material-ui/core';
+import React = require('react');
+
+interface Props {
+	label: string,
+	axisLabels: {
+		min: string,
+		max: string,
+	},
+	onChange: (e: React.ChangeEvent, value: number) => void
+}
+
+const TLXInput: React.FC<Props> = (props) => {
+
+	const defaultValue = 3;
+	React.useEffect(
+		() => {
+			props.onChange(null, defaultValue);
+		},
+		[]
+	);
+
+	return <div className="tlx-input">
+		<span className="input-label">{props.label}</span>
+		<Grid container spacing={2}>
+			<Grid item>
+				{props.axisLabels.min}
+			</Grid>
+			<Grid item>
+				<Slider
+					style={{width: '200px'}}
+					min={1}
+					max={7}
+					step={1}
+					marks={true}
+					defaultValue={defaultValue}
+					onChange={props.onChange}
+				/>
+			</Grid>
+			<Grid item>
+				{props.axisLabels.max}
+			</Grid>
+
+		</Grid>
+	</div>;
+};
+
+export default TLXInput;
